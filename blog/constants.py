90d68f66a9eaf97478c8307d1852d03c4d2ca805
Gareth Coles <gdude2002@gmail.com>
2019-02-22 11:16:22 +0100

    Forget block editor - let's do something possible first

--- blog/constants.py
+++ blog/constants.py
@@ -1,10 +1,1 @@
-from enum import Enum
 
-
-class BlockType(Enum):
-    HEADER = 0
-    PARAGRAPH = 1
-    LIST = 2
-    IMAGE = 3
-    FRAME = 4
-    MEDIA = 5
--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from blog.constants import BlockType
-
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     html = models.TextField()
+    markdown = models.TextField()
     slug = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
+
     pub_date = models.DateTimeField("date published")
 
     def __str__(self):
         return self.title
-
-
-class Block(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    type = models.IntegerField(
-        choices=[(t, t.value) for t in BlockType]
-    )
-    content = models.TextField()
-    order = models.IntegerField()
