faf9b4a4370e74c3c057605f06d469ec65aa43a6
Gareth Coles <gdude2002@gmail.com>
2019-02-26 13:52:28 +0000

    To be safe, let's not redirect to setup from the index

--- base/views/index.py
+++ base/views/index.py
@@ -1,13 +1,7 @@
-from django.contrib.auth.models import User
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 from django.views import View
 
 
 class IndexView(View):
     def get(self, request):
-        users = User.objects.filter(is_staff=True).count()
-
-        if users <= 0:
-            return redirect("base.setup")
-
         return render(request, "base/index.html")
