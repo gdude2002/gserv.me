48a42ff6ff3fba8cd2f6b230ceef690a80a16815
Gareth Coles <gdude2002@gmail.com>
2019-03-01 17:50:10 +0000

    Knew I'd commit this commented-out...

--- base/urls.py
+++ base/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = (
     path("", TemplateView.as_view(template_name="base/index.html"), name="base.index"),
     path('accounts/', include('django.contrib.auth.urls')),
-    path("setup", views.SetupIndexView.as_view(), name="base.setup"),
+    path("setup/", views.SetupIndexView.as_view(), name="base.setup"),
 )
