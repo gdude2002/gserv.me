4186a0649459f1dea7337fc149e3d4b0a6321a0b
Gareth Coles <gdude2002@gmail.com>
2019-03-02 23:38:08 +0000

    I've always been a fan of secondary navbars.
    Now I get to actually make a site that uses 'em!

--- blog/urls.py
+++ blog/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 
 from blog.views.posts import PostsView
 from gserv_me.views.pattern_redirect import PatternRedirectView
-from .views.index import IndexView
 
 
 urlpatterns = (
-    path("", IndexView.as_view(), name="blog.index"),
+    path("", PatternRedirectView.as_view(
+        pattern_name="blog.posts",
+        reverse_kwargs={"page": 1}
+    ), name="blog.index"),
     path("posts", PatternRedirectView.as_view(
         pattern_name="blog.posts",
-        reverse_args=(1, )
+        reverse_kwargs={"page": 1}
     )),
     path("posts/<int:page>", PostsView.as_view(), name="blog.posts"),
 )
